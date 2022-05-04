d240fbc65d236e3f2553f9bf614c3b42c03a2e88
Carlos <carloseduardo.f.p@hotmail.com>
2022-05-04 19:44:48 -0300

    Atualização

--- www/front/js/script-ranking.js
+++ www/front/js/script-ranking.js
@@ -1,36 +1,68 @@
-const rank = document.querySelector('.rank')
+const tableRank = document.querySelector(".table-rank");
 
-const getPosts = async() => {
-    const response = await  fetch(`https://jsonplaceholder.typicode.com/users`)
-    return response.json()
-}
+const getPosts = async () => {
+  const response = await fetch(
+    `https://6259e03a43fda1299a13640f.mockapi.io/Users`
+  );
+  return response.json();
+};
 
-const addPostsIntoDom = async () => {
-    const posts = await getPosts()
-   console.log(posts);
-   const melhores = posts.map(rank => 
-    `<table class="table table-striped">
-      <tbody>
+
+const addPostsIntoDom = (posts) => {
+  const melhores = posts.map(({ Nome, id, XPAtual}) => {
+      return `
         <tr>
-          <th scope="row">${rank.id}</th>
-          <td>${rank.name}</td>
-          <td>${rank.phone}</td>
-        </tr>`
+        <td>${id}</td>
+        <td>${Nome}</td>
+        <td>${XPAtual}</td>
+        </tr>
+        `;
+    }).join("");
+    tableRank.innerHTML += melhores;
+};
 
-     /*`<div class="names">
-     <ul>
-     <li> ${rank.id} ${rank.name} ${rank.phone} </span> </li>
-     </ul> 
-    </div>`*/
-   ).join('')
-   rank.innerHTML = melhores
-
+const addPostsIntoPodium = (podium) => {
+  podium.forEach((item, index) => {
+    console.log('index:', index)
+    document.querySelector(`.top${index + 1} .profile`).src = item.ProfilePic;
+    document.querySelector(`.top${index + 1} .xp-number`).innerHTML = `${item.XPAtual} xp`;
+  })
 }
-addPostsIntoDom()
-const number = [1,2,3,4,5]
 
-const numeroMaior =number.reduce(function(a, b) {
-    return Math.max(a, b);
-  }, -Infinity);
+const sortByXP = ( a, b ) => {
+  if ( a.XPAtual < b.XPAtual ){
+    return 1;
+  }
+  if ( a.XPAtual > b.XPAtual ){
+    return -1;
+  }
+  return 0;
+}
 
-  console.log(numeroMaior);
+
+const Rank3Position = async() => {
+    const response = await getPosts();
+    let array = []
+    // const filtro = top3.filter((teste) => {
+    //     array.push(teste.XPAtual)
+    // })
+    const sorted = response.sort(sortByXP)
+    console.log('sorted:', sorted)
+
+    const [top1, top2, top3, ...rest] = sorted;
+    console.log('top1, top2, top3, ...rest:', top1, top2, top3, rest)
+    addPostsIntoPodium([top1, top2, top3])
+
+    addPostsIntoDom(rest)
+    // let n = 0
+    // const top = top3.filter((teste) => {
+     
+    //     if(teste.XPAtual == sorted[n] ) {
+    //         console.log(teste);
+    //     }
+    //    ;
+    // })
+
+    // console.log(sorted[0],sorted[1], sorted[2]);
+}
+Rank3Position()
