307a8e9f966d0c56bab3d469ef282a8cac8124c8
CesarOncala <cesarh.oncala@gmail.com>
2022-05-17 22:45:47 -0300

    git commit api .net5 -v

---
+++ Api/Gameficacao/Gameficacao/models/Materia.cs
@@ -0,0 +1,14 @@
+﻿using RankingApi.Context;
+using System.Collections.Generic;
+
+namespace Gamificacao.models
+{
+    // Modelo de retorno dos Quizes
+    // 5 matérias, 3 niveis
+
+    public class Materia
+    {
+        public string NomeMateria { get; set; }
+        public List<Quiz> Quizzes { get; set; }
+    }
+}
---
+++ Api/Gameficacao/Gameficacao/models/Ranking.cs
@@ -0,0 +1,14 @@
+namespace Gameficacao.RankingApi
+{
+    public record Ranking
+    {
+        public string? Nome { get; set; }
+
+        public int Id { get; set; }
+
+        public double Nota { get; set; }
+
+    }
+}
+
+
