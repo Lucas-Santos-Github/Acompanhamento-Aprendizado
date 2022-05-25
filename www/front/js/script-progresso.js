decc998e747f24a1a13588b5831125ca5f578b3e
Luan Ferreira <49698412+luanalvesferreira@users.noreply.github.com>
2022-05-25 00:13:47 -0300

    Progresso - Finalizando

--- www/front/js/script-progresso.js
+++ www/front/js/script-progresso.js
@@ -26,6 +26,7 @@ function detalhesUsuario(url) {
 
       document.getElementById('infousuario').innerHTML =
         `
+        
         <div class="container" id="infousuario">
         <div class="container imagemperfil" id="imagemHero" >
           <div class="row">
@@ -60,6 +61,97 @@ function detalhesUsuario(url) {
           console.log(res)
           document.getElementById("barraprogresso").innerHTML =
             `
+            <br>
+      <h2 class="nivel-progresso">Estatísticas</h2>
+      <h4><i class="bi bi-lightning-charge-fill"></i>Atual: ${res.currentRanking}</h5>
+      <h4><i class="bi bi-lightning-charge-fill"></i>Próximo nível: ${res.nextRanking}</h5>
+      
+        <h2 class="nivel-progresso">Nível</h2>
+
+        <div class="a-box-card body-card-perfil">
+            <div class="img-container-card ">
+                <div class="img-inner img-container-card">
+                    <div class="inner-skew-card">
+                        <img
+                            src="https://p4.wallpaperbetter.com/wallpaper/447/658/479/roronoa-zoro-4k-new-hd-pc-wallpaper-thumb.jpg">
+                    </div>
+                </div>
+            </div>
+            <div class="text-container-card">
+                <h3>Nível 1</h3>
+                <div>
+                    Descrição nivel 1.
+                </div>
+            </div>
+        </div>
+
+        <div class="a-box-card body-card-perfil">
+            <div class="img-container-card ">
+                <div class="img-inner img-container-card">
+                    <div class="inner-skew-card">
+                        <img
+                            src="https://besthqwallpapers.com/Uploads/24-4-2018/49759/thumb2-4k-vegeta-fan-art-profile-dragon-ball-z.jpg">
+                    </div>
+                </div>
+            </div>
+            <div class="text-container-card">
+                <h3>Nível 2</h3>
+                <div>
+                    Descrição nivel 2.
+                </div>
+            </div>
+        </div>
+
+        <div class="a-box-card body-card-perfil">
+            <div class="img-container-card ">
+                <div class="img-inner img-container-card">
+                    <div class="inner-skew-card">
+                        <img
+                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgWBfX7C2Z1CRbdgVUPmYiP105LbBi2IBeqw&usqp=CAU">
+                    </div>
+                </div>
+            </div>
+            <div class="text-container-card">
+                <h3>Nível 3</h3>
+                <div>
+                    Descrição nivel 3.
+                </div>
+            </div>
+        </div>
+
+        <div class="a-box-card body-card-perfil">
+            <div class="img-container-card ">
+                <div class="img-inner img-container-card">
+                    <div class="inner-skew-card">
+                        <img
+                            src="https://besthqwallpapers.com/Uploads/2-7-2018/57797/thumb2-ultra-instinct-goku-4k-son-goku-fire-dragon-ball.jpg">
+                    </div>
+                </div>
+            </div>
+            <div class="text-container-card">
+                <h3>Nível 4</h3>
+                <div>
+                    Descrição nivel 4.
+                </div>
+            </div>
+        </div>
+
+        <div class="a-box-card body-card-perfil">
+            <div class="img-container-card ">
+                <div class="img-inner img-container-card">
+                    <div class="inner-skew-card">
+                        <img
+                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxmp59t-tgQDuK_XUpXmUIJYOWXJ-9M7soSg&usqp=CAU">
+                    </div>
+                </div>
+            </div>
+            <div class="text-container-card">
+                <h3>Nível 5</h3>
+                <div>
+                    Descrição nivel 5.
+                </div>
+            </div>
+        </div>
         <div id ="barraprogresso" class="row bs-wizard" style="border-bottom:0;">
         <div class="col-xs-3 bs-wizard-step ${nivelString(res.currentRanking) <= 1 ? "" : "complete"}">
                 <div class="progress">
